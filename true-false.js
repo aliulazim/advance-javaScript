71f114c89532f8402f01efbf2c46c2da8ba5d67b
aliulazim <aliulazimbd@gmail.com>
2021-02-02 12:00:51 +0600

    Truthy and Falsy values

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// False value
+// const name = "";
+// const name;
+// const age = 0;
+// const age = null;
+// const age = NaN;
+// const age = false;
+
+
+// True value
+
+// const name = " ";
+// const name = " shalman ";
+// const name = [];
+// const name = {};
+
+let age = 4;
+let age = -4;
+
+if (name) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
