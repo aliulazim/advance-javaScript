c077b321cccd2dbcd5d66879771e44e55808d3f9
aliulazim <aliulazimbd@gmail.com>
2021-02-02 16:43:39 +0600

    Double equal vs triple equal

---
+++ doubleVStripleEqual.js
@@ -0,0 +1,46 @@
+const num1 = 2;
+const num2 = "2";
+// double equal just check value
+if (num1 == num2) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+const age = 1;
+const age1 = true;
+
+if (age == age1) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+
+// Triple equal check value and type
+if (num1 === num2) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+if (age === age1) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
