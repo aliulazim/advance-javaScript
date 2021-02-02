e0feb5e9e1e771a7fea865ceb1d3960c58819b50
aliulazim <aliulazimbd@gmail.com>
2021-02-02 12:27:24 +0600

    Null Vs Undefined

---
+++ nullVsUndefine.js
@@ -0,0 +1,28 @@
+ Undefined value
+
+ let name;
+
+ function add(num1, num2) {
+     console.log(num1 + num2);
+ }
+
+
+ function add(num1, num2) {
+     console.log(num1 + num2);
+     return;
+ }
+
+ function add(num1, num2) {
+     console.log(num1 + num2);
+ }
+ const result = add(11, );
+
+ const jamalBoi = { name: "md Jamal", fatherName: "faruk" };
+ console.log(jamalBoi.motherName);
+
+
+ let fun = undefined;
+ console.log(fun);
+
+ let ages = [2, 3, 9];
+ console.log(ages[10]);
