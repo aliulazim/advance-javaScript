9badbb7a3b9e59cb7d1bf7eed3be3ba46c0d51b0
aliulazim <aliulazimbd@gmail.com>
2021-02-03 11:38:05 +0600

    Array slice, splice, array join elements

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const part = nums.slice(3, 7);
+// const remove = nums.splice(2, 5, 22);
+// console.log(part);
+// console.log(remove);
+// console.log(nums);
+
+const toGether = nums.join(' ');
+console.log(toGether);
